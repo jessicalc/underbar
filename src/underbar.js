b29cada56ed50cbde1b0d8c4bff701a95a6052eb
jessicalc <jessica@crayoncollective.com>
2015-07-05 10:49:05 +0800

    Started rewriting underscore functions
    * Implemented functions up to _.every (WIP)

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,10 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (n > array.length)
+      return array;
+    else 
+      return n === undefined? array[array.length - 1] : array.slice(array.length - n, array.length);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +50,14 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++)
+        iterator(collection[i], i, collection)
+    } else {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -66,24 +79,51 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+
+    _.each(collection, function(item, index) {
+      if (test(item))
+        result.push(item);
+    });
+
+    return result; 
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function(item) {
+      if (!test(item))
+        return item;
+    })
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var result = [];
+    // it should loop through the input array and see if the item in the new array's
+    // indexOf is -1 (i.e. that it doesn't exist). 
+    // if it doesn't exist, push to result.
+    // if it exists, skip.
+    _.each(array, function(item, index) {
+      if (_.indexOf(result, item) == -1)
+        result.push(item);
+    })
+    return result;
   };
 
 
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
+    var result = [];
+    _.each(collection, function(item, index, collection) {
+      result.push(iterator(item));
+    })
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    return result;
   };
 
   /*
@@ -125,17 +165,30 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    if (accumulator == null)
+      var accumulator = collection.shift();
+    else
+      var accumulator = accumulator;
+    _.each(collection, function(item, index, collection) {
+      accumulator = iterator(accumulator, item)
+    })
+    return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
   _.contains = function(collection, target) {
     // TIP: Many iteration problems can be most easily expressed in
     // terms of reduce(). Here's a freebie to demonstrate!
+
+    // **NB** I don't understand how this works?
+    // wasFound is the accumulator, and by default it's set to false (see)
+    // (the third argument, false). 
+
     return _.reduce(collection, function(wasFound, item) {
-      if (wasFound) {
-        return true;
-      }
-      return item === target;
+      if (wasFound) {             // so the stuff 
+        return true;              // in here
+      }                           // is the
+      return item === target;     // accumulator? 
     }, false);
   };
 
