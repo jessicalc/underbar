b769da66932301cde81b7b14b37602837e0e2823
jessicalc <jessica@crayoncollective.com>
2015-07-27 01:11:21 +0800

    Made _.invoke pass

--- src/underbar.js
+++ src/underbar.js
@@ -367,6 +367,9 @@
   // Calls the method named by functionOrKey on each value in the list.
   // Note: You will need to learn a bit about .apply to complete this.
   _.invoke = function(collection, functionOrKey, args) {
+    return _.map(collection, function(item, index, collection) {
+        return functionOrKey.apply(item, args);
+    })
   };
 
   // Sort the object's values by a criterion produced by an iterator.
